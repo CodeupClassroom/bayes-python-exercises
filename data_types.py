73a23f271c0b49241b6de6fae3a31b395f566a22
RyanOrsinger <ryan.orsinger@gmail.com>
2019-09-10 16:33:25 -0500

    add immutability example

--- data_types.py
+++ data_types.py
@@ -13,12 +13,21 @@ data_types = [
 ]
 
 # list of lists
-[[1, 2], [3, 4], [5, 6]]
+x = [[1, 2], [3, 4], [5, 6]]
+print(x)
+print(type(x))
+print(type(x[0]))
 
 # tuple of tuples -> immutable
-((1, 2), (3, 4))
+x= ((1, 2), (3, 4))
+print(x)
+print(type(x))
+print(type(x[0]))
 
 # string are immutable
+fruit = "banana"
+print(fruit[0]) # points to the first character
+fruit[0] = "B" # we cannot reassign pieces of a string because strings are immutable 
 
 # list of dictionaries are super common
 
@@ -39,4 +48,3 @@ applications_where_python_is_used_heavily = [
 for application in applications_where_python_is_used_heavily:
     print(applications_where_python_is_used_heavily)
 
-    
