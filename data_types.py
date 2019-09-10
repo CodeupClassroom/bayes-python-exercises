efcfe1c94f82fe3af8427d6234cf5f9778042ac0
RyanOrsinger <ryan.orsinger@gmail.com>
2019-09-10 16:26:37 -0500

    add operators and data_types

---
+++ data_types.py
@@ -0,0 +1,42 @@
+data_types = [
+    "bool"
+    "int"
+    "list"
+    "string"
+    "float"
+    "dictionary"
+    "tuple"
+    "set"
+    "function"
+    "None"
+    "class"
+]
+
+# list of lists
+[[1, 2], [3, 4], [5, 6]]
+
+# tuple of tuples -> immutable
+((1, 2), (3, 4))
+
+# string are immutable
+
+# list of dictionaries are super common
+
+
+applications_where_python_is_used_heavily = [
+    "Popular spaces/applications for Python"
+    "Life Sciences + Hard sciences"
+    "Data Science (ML, Deep Learning, Computer vision)"
+    "Cyberdefense & systems hardening"
+    "Systems automation"
+    "Dev-Ops (intersection of programming and infrastructure) - software defined infrastructure"
+    "Robotics"
+    "Edge computing (IOT)"
+    "Web Development"
+    "Building APIs + services"
+]
+
+for application in applications_where_python_is_used_heavily:
+    print(applications_where_python_is_used_heavily)
+
+    
---
+++ operators.py
@@ -0,0 +1,8 @@
+# Operators that give us booleans
+"a" == "A" (False)
+2 > 1   (True)
+3 <= 2  (False)
+pizza = "piña"
+pizza != "pineapple"
+"b" in "banana"
+"c" not in "mango"
